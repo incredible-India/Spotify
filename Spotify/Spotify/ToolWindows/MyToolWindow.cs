58c50c5b199c0e0b48c15378de5ed3caf75f3788
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-12-24 22:30:40 +0530

    Spotify code added

---
+++ Spotify/Spotify/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Spotify
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Spotify";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("cb1c8a25-12e4-407c-89f1-fd6499d7237c")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ Spotify/Spotify/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Spotify
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        public MyToolWindowControl()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private async void InitializeAsync()
+        {
+            Random rnd = new Random();
+            string subFolder = rnd.Next().ToString();
+            var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync(userDataFolder: Path.Combine(Path.GetTempPath(), $"{Environment.UserName}", subFolder));
+            await Spotify.EnsureCoreWebView2Async(env);
+            var request = Spotify.CoreWebView2.Environment.CreateWebResourceRequest("https://open.spotify.com/", "GET", null, null);
+            Spotify.CoreWebView2.NavigateWithWebResourceRequest(request);
+        }
+    }
+}
